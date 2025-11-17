3eee72d7c871bb6aad4d9ba6faea5eeaf3caa944
vladcalin-edw <calin.vlad@eaudeweb.ro>
2025-11-17 10:17:02 +0200

    update tests

--- test/tests/plone-addons/run.sh
+++ test/tests/plone-addons/run.sh
@@ -2,6 +2,6 @@
 set -eo pipefail
 
 docker run -i --rm \
-	-e ADDONS="eea.api.controlpanel" \
+	-e ADDONS="eea.facetednavigation" \
 	-e PIP_PARAMS="-q --disable-pip-version-check" \
-	"$1" bin/python -c "from eea import controlpanel; print(controlpanel.__name__)"
+	"$1" bin/python -c "from eea import facetednavigation; print(facetednavigation.__name__)"
